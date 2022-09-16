e349eaaf70d450004daf7003ae74de981f2d836f
ailopez- <aitorlpzaudikana@gmail.com>
2022-09-16 12:41:36 +0200

    scripting

--- eval_scripts/numops.sh
+++ eval_scripts/numops.sh
@@ -1,13 +1,25 @@
 echo "\n******* 500 Num Ops Test *********"
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
-./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l
+var1=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var1
+var2=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var2
+var3=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var3
+var4=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var4
+var5=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var5
+var6=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var6
+var7=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var7
+var8=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var8
+var9=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var9
+var10=$(./push_swap $(ruby -e "puts (-249..250).to_a.shuffle.join(' ')") | wc -l)
+echo $var10
+
+var11=$(( var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var9 + var10 ))
+var11=$(( var11 /10))
+echo "--> $var11\n"
