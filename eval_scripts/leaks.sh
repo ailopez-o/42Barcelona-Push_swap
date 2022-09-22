8d8fc269aec93aef7e020495039e5464a976af4d
ailopez-o <ailopez-@student.42barcelona.com>
2022-09-22 15:44:58 +0200

    leaks test

--- eval_scripts/leaks.sh
+++ eval_scripts/leaks.sh
@@ -1,1 +1,5 @@
 leaks --atExit -- ./push_swap $(ruby -e "puts (-50..50).to_a.shuffle.join(' ')")
+
+ARG=$(ruby -e "puts (0..500).to_a.shuffle.join(' ')"); 
+echo "\n******* CHECKER *********\n"
+./push_swap $ARG | leaks --atExit -- ./my_checker $ARG
--- eval_scripts/my_cheker.sh
+++ eval_scripts/my_cheker.sh
@@ -1,5 +1,4 @@
 echo "\n******* Checker Test *********"
 ARG=$(ruby -e "puts (0..500).to_a.shuffle.join(' ')"); 
-#./push_swap $ARG 
 echo "\n******* CHECKER *********\n"
 ./push_swap $ARG | ./my_checker $ARG
